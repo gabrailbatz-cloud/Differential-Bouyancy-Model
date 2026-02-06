2a5ba953db95832c19efad99a300a42caa4867ec
gabrailbatz-cloud <gabrailbatz@gmail.com>
2026-02-06 11:00:15 -0800

    Delete differential-buoyancy-model directory

--- differential-buoyancy-model/src/differential_buoyancy_model/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Differential buoyancy model (simple vertical motion in water)."""
-
-from .model import Params, simulate, plot_position, animate_position_plotly
-
-__all__ = ["Params", "simulate", "plot_position", "animate_position_plotly"]
